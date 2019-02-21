3e2361184e647e40bf68f8ed6298df3550129962
Ali Tayyip Aydın <alitayyipaydin@hotmail.com>
2019-02-21 23:14:37 +0300

    V1.0
    kayıplı gizleme..

---
+++ Stegonography V1.0/Stegonography V1.0/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Stegonography_V1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(170, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Resim Eklemek İçin TIklayın";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(314, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Resim Eklemek İçin TIklayın";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 304);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(262, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 304);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(506, 65);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Birleştir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(835, 427);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Silver;
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.pictureBox3);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(827, 401);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Resimleri Birleştir";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(519, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(299, 65);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Resimi Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(518, 19);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(301, 304);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Silver;
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.pictureBox5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.pictureBox4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(827, 401);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Resmi Çöz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(539, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Gizlenmiş Resim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Şifreli Rsim";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(542, 20);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(276, 375);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 3;
+            this.pictureBox5.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(288, 233);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(248, 79);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Gizli Resmi Çıkar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(288, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(248, 79);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Resmi Değiştir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(6, 20);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(276, 375);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 436);
+            this.Controls.Add(this.tabControl1);
+            this.MaximumSize = new System.Drawing.Size(862, 475);
+            this.MinimumSize = new System.Drawing.Size(862, 475);
+            this.Name = "Form1";
+            this.Text = "Stegonography";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Stegonography V1.0/Stegonography V1.0/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stegonography_V1._0
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap image3;
+        string DosyaYolu1="";
+        string DosyaYolu2="";
+        string DosyaYolu3 = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+#region Taban Dönüşümü
+
+        
+        string sayi_dan_binary(int sayi1)
+        {
+            int sayi, kalan;
+            string yazikalan = "";
+            sayi = int.Parse(sayi1.ToString());
+            while (sayi != 0)
+            {
+                kalan = sayi % 2;
+                sayi = sayi / 2;
+                yazikalan = kalan.ToString() + yazikalan;
+            }
+            for (int i = yazikalan.Length; i < 8; i++)
+            {
+                yazikalan = "0" + yazikalan;
+            }
+            return yazikalan;
+        }
+
+        int binary_den_sayi(string binary)
+        {
+            int sayi = 0;
+            for (int i = 7; i >= 0; i--)
+            {
+                if (binary[i].ToString() == "1")
+                {
+                    sayi += Convert.ToInt32(Math.Pow(2, ((i - 7) * -1)));
+                }
+            }
+            return sayi;
+        }
+
+
+        #endregion
+
+#region fotoğrafı diğerinin ardına gizleme
+
+        void resimleribirlestir()
+        {
+            try
+            {
+
+                Bitmap image1;
+                Bitmap image2;
+                MessageBox.Show("Resim Gizlenmeye başlıyor");
+                
+                     image1 = new Bitmap(pictureBox1.Image);
+                     image2 = new Bitmap(pictureBox2.Image);
+             
+                
+                image3 = new Bitmap(image1.Width, image1.Height);
+
+                int x, y;
+
+                    for (x = 0; x < image1.Width; x++)
+                    {
+                        for (y = 0; y < image1.Height; y++)
+                        {
+                            Color pixelColor1 = image1.GetPixel(x, y);
+                            Color pixelColor2 = image2.GetPixel(x, y);
+
+                        #region son 3 bit değişimi
+
+                        // string alpha = sayi_dan_binary(pixelColor2.R).Substring(3,2)+ sayi_dan_binary(pixelColor2.G).Substring(3, 2)+ sayi_dan_binary(pixelColor2.B).Substring(3, 2) +"00";
+
+                           // MessageBox.Show(pixelColor1.GetHue().ToString());
+                            
+
+                            string red = sayi_dan_binary(pixelColor1.R).Substring(0, 5);
+                            //red = red.Remove(4,3);
+                            red += sayi_dan_binary(pixelColor2.R).Substring(0, 3);
+
+                            string green = sayi_dan_binary(pixelColor1.G).Substring(0, 5);
+                            //green = green.Remove(4, 3);
+                            green += sayi_dan_binary(pixelColor2.G).Substring(0, 3);
+
+                            string blue = sayi_dan_binary(pixelColor1.B).Substring(0, 5);
+                            //blue = blue.Remove(4, 3);
+                            blue += sayi_dan_binary(pixelColor2.B).Substring(0, 3);
+                            
+
+                            #endregion
+
+                            Color newColor = Color.FromArgb(binary_den_sayi(red), binary_den_sayi(green), binary_den_sayi(blue));
+                            
+                            image3.SetPixel(x, y, newColor);
+                        }
+                    }
+
+                pictureBox3.Image = image3;
+
+                pictureBox4.Image = image3;
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+#endregion
+        //psnr değeri yaz!!
+
+
+#region Gizli fotoğrafı çıkartma
+
+
+        void cozme(Bitmap image1)
+        {
+            int x = 0, y = 0;
+            MessageBox.Show("Gizli Resim Çıkarılmaya başlıyor");
+
+            for (x = 0; x < image1.Width; x++)
+            {
+                for (y = 0; y < image1.Height; y++)
+                {
+
+                    Color pixelColor1 = image1.GetPixel(x, y);
+
+                    string red = sayi_dan_binary(pixelColor1.R).Substring(5, 3) + "00000";
+                    string green = sayi_dan_binary(pixelColor1.G).Substring(5, 3) + "00000";
+                    string blue = sayi_dan_binary(pixelColor1.B).Substring(5, 3) + "00000";
+
+                    Color newColor = Color.FromArgb(binary_den_sayi(red), binary_den_sayi(green), binary_den_sayi(blue));
+
+                    image1.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox5.Image = image1;
+        }
+       // Orjinal = new Bitmap(pictureBox1.Image);
+
+
+#endregion
+
+
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Resim Dosyası .png|*.jpg";
+            file.ShowDialog();
+            DosyaYolu1 = file.FileName;
+            pictureBox2.ImageLocation = DosyaYolu1;
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Resim Dosyası .png|*.jpg";
+            file.ShowDialog();
+             DosyaYolu2 = file.FileName;
+            pictureBox1.ImageLocation = DosyaYolu2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image!=null&&pictureBox2.Image!=null)
+            {
+                if (pictureBox1.Image.Size==pictureBox2.Image.Size)
+                {
+                    resimleribirlestir();
+                }
+                else
+                {
+                    MessageBox.Show("Eşit Boyutta Resimler Giriniz.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seçilmeyen resim var!!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Kaydeymek için
+            if (pictureBox3.Image!=null)
+            {
+                SaveFileDialog savedialog = new SaveFileDialog();
+                if (savedialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox3.Image.Save(savedialog.FileName + ".jpg", ImageFormat.Jpeg);
+                }
+            }
+       }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Resim Dosyası .png|*.jpg";
+            file.ShowDialog();
+            DosyaYolu3 = file.FileName;
+            pictureBox4.ImageLocation = DosyaYolu3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox4.Image!=null)
+            {
+                Bitmap image1 = new Bitmap(pictureBox4.Image);
+                cozme(image1);
+            }
+            else
+            {
+                MessageBox.Show("bir resim seçiniz");
+            }
+        }
+    }
+}
